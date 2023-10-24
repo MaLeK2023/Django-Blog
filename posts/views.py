faf54f981f7f89c19e91c2dfa46eb3d9e68d999b
MaLeK2023 <m.alsheikh001@gmail.com>
2023-10-24 20:26:07 +0200

    add class based views

--- posts/views.py
+++ posts/views.py
@@ -1,47 +1,32 @@
 from django.shortcuts import render , redirect
 from .models import Post 
 from .form import PostForm 
+from django.views import generic
 
 # Create your views here.
-def post_list(request):
-    data = Post.objects.all()
-    return render(request,'posts/posts.html',{'posts':data})
-
-def post_detail(request,post_id):
-    data = Post.objects.get(id=post_id)
-    return render(request,'posts/detail.html',{'post':data})
 
 
-def new_post(request):
-    if request.method == 'POST':
-    
-         form = PostForm(request.POST,request.FILES)
-         if form.is_valid():
-             form.save()
-             return redirect('/blog/')
+class PostList(generic.ListView): # html post_list , context object_list post_list
+    model = Post
 
-    else: 
-        form = PostForm()
-
-    return render(request,'posts/new.html',{'form':form})
+class PostDetail(generic.DetailView): # html post_detail , context object post
+    model = Post
 
 
+class PostCreate(generic.CreateView):
+    model = Post
+    fields = ['title','content','image','tags']
+    success_url = '/blog/'
 
-def edit_post(request,post_id):
-    data = Post.objects.get(id=post_id)
-    if request.method == 'POST':
-    
-         form = PostForm(request.POST,request.FILES,instance=data)
-         if form.is_valid():
-             form.save()
-             return redirect('/blog/')
 
-    else: 
-        form = PostForm(instance=data)
+class PostEdit(generic.UpdateView):
+    model = Post
+    fields = ['title','content','image','tags']
+    success_url = '/blog/'
+    template_name = 'posts/edit.html'
 
-    return render(request,'posts/edit.html',{'form':form})
 
-def delete_post(request,post_id):
-    data = Post.objects.get(id=post_id)
-    data.delete()
-    return redirect('/blog/')
+class PostDelete(generic.DeleteView):
+    model = Post
+    success_url = '/blog/'
+
