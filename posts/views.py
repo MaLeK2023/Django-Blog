d42b0e2f66650511f9d8ce9cbd40c9874f41443f
MaLeK2023 <m.alsheikh001@gmail.com>
2023-10-24 21:14:14 +0200

    use mixins

--- posts/views.py
+++ posts/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render , redirect
 from .models import Post 
 from .form import PostForm 
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 
 
-class PostList(generic.ListView): # html post_list , context object_list post_list
+class PostList(LoginRequiredMixin,generic.ListView): # html post_list , context object_list post_list
     model = Post
+    login_url = '/admin/login'
+    
 
 class PostDetail(generic.DetailView): # html post_detail , context object post
     model = Post
